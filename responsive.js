f5441f41ff3a8119dc8972e71466c320168c9d49
rayreham <rayreham@yahoo.com>
2020-08-10 18:45:00 -0400

    pushing javascript file for responsive features

---
+++ responsive.js
@@ -0,0 +1,64 @@
+ //this function in java script is used to navigate the page when clicking elements in bar
+function scrolling() {
+	scrollPos = $(this).scrollTop();
+	$('.p1').css({
+		'background-position' : '50% ' + (-scrollPos/4)+"px"
+	});
+  $('.p2').css({
+		'background-position' : '50% ' + (-scrollPos/8)+"px"
+	});
+  $('.p3').css({
+		'background-position' : '70% ' + (-scrollPos/16)+"px"
+	});
+	$('.parallax-text').css({
+		'margin-top': (scrollPos/2)+"px",
+		'opacity': 1-(scrollPos/230)
+	});
+}
+$(document).ready(function(){
+	$(window).scroll(function() {
+		scrolling();
+	});
+});
+
+ //JavaScript for the photo function that changes layout
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+// Full-width images
+function one() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "100%";  // IE10
+    elements[i].style.flex = "100%";
+  }
+}
+
+// Two images side by side
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "50%";  // IE10
+    elements[i].style.flex = "50%";
+  }
+}
+
+// Four images side by side
+function three() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";  // IE10
+    elements[i].style.flex = "25%";
+  }
+}
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myHeader");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
